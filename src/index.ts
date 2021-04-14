2147255b26acc4c1dac1a060ee32a604a4bf2af8
Wachira Norasing <wachira_norasing@outlook.com>
2021-04-15 03:56:37 +0700

    add new client

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
 import prisma from './lib/prisma'
-const PORT = process.env.PORT || 3001
+import path from 'path'
+// import https from 'https'
+// import fs from 'fs'
+
+const HTTP_PORT = process.env.HTTP_PORT || 8080
+// const HTTPS_PORT = process.env.HTTPS_PORT || 8443
 const server = express()
 
 server.use(express.urlencoded({ extended: true }))
@@ -13,20 +18,35 @@ server.use(
     ':remote-addr :remote-user :method :url HTTP/:http-version :status :res[content-length] - :response-time ms'
   )
 )
-server.use(cors())
+server.use(cors({origin: '35.240.156.20'}))
 
 server.use('/api', router)
+server.use('/', express.static(path.join(__dirname, 'dist')))
 
-server.use('/', (req, res) => {
-  res.send('Ragnarok Hell -- Server')
-})
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync('privkey.pem'),
+//       cert: fs.readFileSync('fullchain.pem'),
+//     },
+//     server
+//   )
+//   .listen(HTTPS_PORT, async () => {
+//     try {
+//       await prisma.$connect()
+//       console.log(`Database is connected to the server`)
+//     } catch (error) {
+//       console.log(`Server can't connect to database.`)
+//     }
+//     console.log(`server run at PORT:${HTTPS_PORT}`)
+//   })
 
-server.listen(PORT, async () => {
+server.listen(HTTP_PORT, async () => {
   try {
     await prisma.$connect()
     console.log(`Database is connected to the server`)
   } catch (error) {
     console.log(`Server can't connect to database.`)
   }
-  console.log(`server run at PORT:${PORT}`)
+  console.log(`server run at PORT:${HTTP_PORT}`)
 })
