d71219fc1dc406528b83f1c7d849cd49c5d2731b
Wachira Norasing <wachira_norasing@outlook.com>
2021-04-15 03:17:47 +0700

    update env

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import express from 'express'
 import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
-const PORT = 3001
+import prisma from './lib/prisma'
+const PORT = process.env.PORT || 3001
 const server = express()
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
 server.use(
-  morgan(':remote-addr :remote-user :method :url HTTP/:http-version :status :res[content-length] - :response-time ms')
+  morgan(
+    ':remote-addr :remote-user :method :url HTTP/:http-version :status :res[content-length] - :response-time ms'
+  )
 )
 server.use(cors())
 
@@ -18,6 +21,12 @@ server.use('/', (req, res) => {
   res.send('Ragnarok Hell -- Server')
 })
 
-server.listen(PORT, () => {
-  console.log(`Server run at PORT:${PORT}`)
+server.listen(PORT, async () => {
+  try {
+    await prisma.$connect()
+    console.log(`Database is connected to the server`)
+  } catch (error) {
+    console.log(`Server can't connect to database.`)
+  }
+  console.log(`server run at PORT:${PORT}`)
 })
